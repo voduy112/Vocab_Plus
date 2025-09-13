c7aadf14c8bdebc3436829a7674bd2c862dbaca4
voduy112 <duyvo1122003@gmail.com>
2025-09-13 22:09:54 +0700

    khởi tạo các tab chính

---
+++ app/lib/features/desks/views/desk_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DeskScreen extends StatelessWidget {
+  const DeskScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Desk'),
+      ),
+    );
+  }
+}
