6ebba2515031998ad09556f3fbc8509f77637a1d
voduy112 <duyvo1122003@gmail.com>
2025-09-17 21:21:50 +0700

    Dùng MultiProvider

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -2,15 +2,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/auth/auth_controller.dart';
-import 'core/widgets/animated_splash_screen.dart';
 import 'router/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AuthController()..listenAuth(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthController()..listenAuth()),
+      ],
       child: Builder(
         builder: (context) {
           final router = createRouter(context.read<AuthController>());
