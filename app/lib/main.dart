caad8e3bc3d9b7a21508feaa5eab039fea856542
voduy112 <duyvo1122003@gmail.com>
2025-11-06 11:46:31 +0700

    chỉnh cho màn hình cố định dọc

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,10 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 import 'app.dart';
-import 'core/widgets/animated_splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,22 +13,13 @@ void main() async {
   FlutterNativeSplash.preserve(
       widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
 
+  // Lock orientation to portrait only
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const SplashApp());
-}
-
-class SplashApp extends StatelessWidget {
-  const SplashApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const AnimatedSplashScreen(),
-      routes: {
-        '/tabs/main': (context) => const App(),
-      },
-    );
-  }
+  runApp(const App());
 }
